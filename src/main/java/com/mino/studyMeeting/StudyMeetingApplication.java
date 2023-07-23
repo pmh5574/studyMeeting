ea7ef1760c88377055495a65f63be7807028e770
pmh5574 <g_a_m_e_@naver.com>
2023-07-23 17:11:35 +0900

    Initial commit

---
+++ src/main/java/com/mino/studyMeeting/StudyMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.mino.studyMeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mino/studyMeeting/StudyMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mino.studyMeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
