450cb1503964e80724c0685d7d6dcb97a916e102
pmh5574 <g_a_m_e_@naver.com>
2023-08-04 17:24:40 +0900

    jpa 추가와 게시판 테스트

---
+++ src/test/java/com/mino/studyMeeting/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.mino.studyMeeting.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 qhsans";
+
+        postsRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("test@test.com")
+                        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
