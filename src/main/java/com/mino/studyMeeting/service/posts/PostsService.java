1ee622e2e2241e79c5929f39bc39a136558511f3
pmh5574 <g_a_m_e_@naver.com>
2023-08-14 22:04:42 +0900

    update 추가

--- src/main/java/com/mino/studyMeeting/service/posts/PostsService.java
+++ src/main/java/com/mino/studyMeeting/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.mino.studyMeeting.service.posts;
 
+import com.mino.studyMeeting.domain.posts.Posts;
 import com.mino.studyMeeting.domain.posts.PostsRepository;
+import com.mino.studyMeeting.web.dto.PostsResponseDto;
 import com.mino.studyMeeting.web.dto.PostsSaveRequestDto;
+import com.mino.studyMeeting.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,4 +19,20 @@ public class PostsService {
         return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
     }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
 }
